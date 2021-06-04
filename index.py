850a095b2e39342fbddf2e5c8fa24929d9d6196d
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-03 23:40:19 -0500

    loops

--- index.py
+++ index.py
@@ -17,7 +17,6 @@ def test(n):
 
 # Arithmetic Operators
 # The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
-
 # The first line contains the sum of the two numbers.
 # The second line contains the difference of the two numbers (first - second).
 # The third line contains the product of the two numbers.
@@ -27,3 +26,20 @@ def arithmetic_operators(a: int, b: int):
     print(a + b)
     print(a - b)
     print(a * b)
+
+# Python: Division
+# The provided code stub reads two integers,  and , from STDIN.
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+# No rounding or formatting is necessary.
+
+
+def division(a: int, b: int):
+    print(a // b)
+    print(a / b)
+
+# Loops
+
+
+def loops(n):
+    for number in range(n):
+        print(pow(number, 2))
