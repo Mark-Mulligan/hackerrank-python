9cce53ff30c4564b374589426705827537d08d47
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-06-03 23:32:31 -0500

    Aritmetic Operators

--- index.py
+++ index.py
@@ -14,3 +14,16 @@ def test(n):
         print('Weird')
     elif n % 2 == 0 and n > 20:
         print('Not Weird')
+
+# Arithmetic Operators
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+
+
+def arithmetic_operators(a: int, b: int):
+    print(a + b)
+    print(a - b)
+    print(a * b)
